84fb9e1d532bc2555defc946d9889b955aaed7e0
Ahmad Hassan <ahmhass313786@gmail.com>
2025-07-29 12:18:10 +0500

    update

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -43,13 +43,23 @@ const placeOrderRazorpay = async (req,res) => {
 // All orders data for admin panel
 
 const allOrders = async (req,res) => {
-
+// 11:37:24
 }
 
 // User order data for frontend
 
 const userOrders = async (req,res) => {
+    try {
+        
+        const {userId} = req.body
+        const orders = await orderModel.find({userId})
+        res.json({success:true,orders})
 
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 }
 
 // Update order status from admin panel
